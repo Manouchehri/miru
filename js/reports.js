1dda19dd35eb2fbb091f5c908402517f57327fd3
Zack Mullaly <zsck@riseup.net>
2017-03-08 20:10:34 -0500

    tidy up the display of reports some more. color reports based on the severity of change reported

--- js/reports.js
+++ js/reports.js
@@ -7,41 +7,21 @@ const contentChange = 'Content Change'
 const rewritten = 'Major Rewrite'
 const deleted = 'Deleted Page'
 
-const green = '#00ff00'
-const yellow = '#ffff00'
-const red = '#ff0000'
+const green = 'rgb(174, 255, 139)'
+const yellow = 'rgb(255, 236, 131)'
+const red = 'rgb(255, 134, 149)'
 
-// Color each report's background either:
-// 1. Green if the change to the site is minor
-// 2. Yellow if there was a change worth inspecting
-// 3. Red if a dramatic rewrite or deletion was detected
-let reports = $('#monitorreports')
-for (let report of reports.children()) {
-    console.log('Inspecting report', report)
-    let rows = reports.children('tbody tr')
-    console.log('Found rows', rows)
-    let siteChangeRows = rows.filter((row) => {
-        console.log('Inspecting row', row)
-        return row
-            .children('td.reportKey')
-            .text()
-            .includes('Site change')
-    })
-    console.log('Found site change rows', siteChangeRows)
-    if (siteChangeRows.length === 0) {
-        continue
-    }
-    let siteChange = siteChangeRows[0]
-        .children('td.reportValue')
-        .text()
-    console.log('Found site change', siteChange)
-    if (siteChange.includes(noChange) || siteChange.includes(minorUpdate)) {
-        report.style('background-color', green)
-    } else if (siteChange.includes(contentUpdate)) {
-        report.style('background-color', yellow)
+$('#monitorreports').children('.reportsummary').each(function () {
+    let _this = $(this)
+    _this.css('background-color', red)
+    let change = $(_this.find('.change')[0]).text()
+    if (change.includes(noChange)) {
+        _this.css('background-color', green)
+    } else if (change.includes(minorUpdate) || change.includes(contentChange)) {
+        _this.css('background-color', yellow)
     } else {
-        report.style('background-color', red)
+        _this.css('background-color', red)
     }
-}
+})
 
 })()
