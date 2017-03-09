ddef09c881fc9e7a9af072e4203a78cc18bd1f09
Zack Mullaly <zsck@riseup.net>
2017-03-08 20:23:36 -0500

    Hide and show the contents of reports when one is clicked

--- js/reports.js
+++ js/reports.js
@@ -11,6 +11,8 @@ const green = 'rgb(174, 255, 139)'
 const yellow = 'rgb(255, 236, 131)'
 const red = 'rgb(255, 134, 149)'
 
+// Set the background color of each report based on the severity
+// of the change detected on the site.
 $('#monitorreports').children('.reportsummary').each(function () {
     let _this = $(this)
     _this.css('background-color', red)
@@ -22,6 +24,25 @@ $('#monitorreports').children('.reportsummary').each(function () {
     } else {
         _this.css('background-color', red)
     }
+})
+
+// Attach a handler to each report that will display its contents when
+// clicked a first time, and hide them the second time.
+$('.moreinfo').each(function () {
+    let _this = $(this)
+    _this.css('visibility', 'hidden')
+    _this.parent().css('height', '40px')
+    let visible = false
+    _this.parent().click(function (target) {
+        visible = !visible
+        if (visible) {
+            _this.css('visibility', 'visible')
+            _this.parent().css('height', 'inherit')
+        } else {
+            _this.css('visibility', 'hidden')
+            _this.parent().css('height', '40px')
+        }
+    })
 })
 
 })()
